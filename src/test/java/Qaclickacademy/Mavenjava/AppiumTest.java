41f677153c0e9db337fb64e78e8cd8db4fecd9ed
Phani <jpkrishna2@gmail.com>
2021-05-09 22:23:00 +0530

    first commit

---
+++ src/test/java/Qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,20 @@
+package Qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	//@Test
+	public void PostJIra()
+	{
+		System.out.println("PostJIra");
+	}
+	
+	@Test
+	public void ElementsUI()
+	{
+		System.out.println("ElementsUI");
+	}
+
+
+}
---
+++ src/test/java/Qaclickacademy/Mavenjava/RestApiTest.java
@@ -0,0 +1,19 @@
+package Qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("NativeAppAndroid");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+
+}
